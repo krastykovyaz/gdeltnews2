96476d54263f902c2438326a105d24ac56656141
krastykovyaz <krasty_90@mail.ru>
2026-02-17 19:43:10 +0000

    :revert "setted to technologies"
    This reverts commit 5a577b84b573db1be1db4c0bac330a10dec116df.

--- main.py
+++ main.py
@@ -7,29 +7,26 @@ def collect_and_update(delta_date_start=1, interval=60):
     # Instantiate the GdeltParser and GdeltDBSaver
     
     while True:
-        try:
         
-            print("Collecting new data...")
-            # Collect data with GdeltParser
-            db_saver = GdeltDBSaver()
-            if len(db_saver.get_all_not_posted()) < 5:
-                parser = GdeltParser(delta_date_start)
-            # Fetch and update one record in the database
-            print("Checking for 'new' records to update...")
-            db_saver = GdeltDBSaver()
-            updated_row = db_saver.mark_as_posted()
-            db_saver.close()
+        print("Collecting new data...")
+        # Collect data with GdeltParser
+        db_saver = GdeltDBSaver()
+        if len(db_saver.get_all_not_posted()) < 5:
+            parser = GdeltParser(delta_date_start)
+        # Fetch and update one record in the database
+        print("Checking for 'new' records to update...")
+        db_saver = GdeltDBSaver()
+        updated_row = db_saver.mark_as_posted()
+        db_saver.close()
 
-            if updated_row:
-                print(f"Updated entry: {updated_row}")
-            else:
-                print("No new entries to mark as 'posted'.")
-            
-            # Wait for the specified interval before the next run
-            print(f"Sleeping for {interval} seconds...")
-            time.sleep(interval)
-        except Exception as e:
-            print(e)
+        if updated_row:
+            print(f"Updated entry: {updated_row}")
+        else:
+            print("No new entries to mark as 'posted'.")
+        
+        # Wait for the specified interval before the next run
+        print(f"Sleeping for {interval} seconds...")
+        time.sleep(interval)
 
 
 if __name__ == "__main__":
